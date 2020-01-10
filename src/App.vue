bfc1907bfe25197f1ea0911c85ddbf09267666c8
Pankaj Boruah <pankaj.boruah@upgrad.com>
2020-01-10 15:53:12 +0530

    project complete

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,45 @@
 <template>
     <div class="container">
-        <h1>Just the initial boilerplate</h1>
-        <p>Switch to branches to get useful content</p>
-        <hr>
-        <ul>
-            <li>
-                <h4>topic/using-slot-component</h4>
-                <p>Shows the use case of slot and component vue elements</p>
-            </li>
-        </ul>
-        
+        <app-header :maxCount="maxQuotes" :quoteCount="quotes.length"/>
+        <app-new-quote @addQuote="addNewQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a quote to delete it!</div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import QuoteGrid from './components/QuoteGrid.vue'
+import NewQuote from './components/NewQuote.vue'
+import Header from './components/Header.vue'
     export default {
-        
+        data:function(){
+            return{
+                quotes:[
+                    'A happy quote'
+                ],
+                maxQuotes: 10
+            }
+        },
+        components:{
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+            appHeader: Header
+        },
+        methods:{
+            addNewQuote(quote){
+                if(this.quotes.length >= this.maxQuotes){
+                    return alert('Please delete some quotes first!')
+                }
+                this.quotes.push(quote);
+            },
+            deleteQuote(i){
+                this.quotes.splice(i,1);
+            }
+        }
     }
 </script>
 
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <app-quote v-for="(quote,i) in quotes" :key=i @click.native="deleteQuote(i)">{{ quote }}</app-quote>
+    </div>
+</template>
+
+<script>
+import Quote from './Quote.vue'
+export default {
+    props:['quotes'],
+    components:{
+        appQuote: Quote
+    },
+    methods:{
+        deleteQuote(i){
+            this.$emit("quoteDeleted", i)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
